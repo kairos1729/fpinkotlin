f45da751d3f6b870455a12410902fb7e5a7e1e40
dominic.godwin <dominic.godwin@rea-group.com>
2022-10-07 18:59:40 +1100

    Exercise 2.1

--- src/test/kotlin/chapter2/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter2/exercises/ex1/listing.kt
@@ -3,18 +3,31 @@ package chapter2.exercises.ex1
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import kotlinx.collections.immutable.persistentMapOf
-import utils.SOLUTION_HERE
+import kotlin.system.measureTimeMillis
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
     //tag::init[]
-    fun fib(i: Int): Int =
+    fun fib(i: Int): Int {
+        tailrec fun go(n: Int, fn: Int, fn1: Int): Int =
+            when {
+                n >= i -> fn
+                else -> go(n + 1, fn1, fn + fn1)
+            }
+        return go(0, 0, 1)
+    }
 
-        SOLUTION_HERE()
-    //end::init[]
+    fun fib_non_tail_recursive(i: Int): Int =
+        when (i) {
+            0 -> 0
+            1 -> 1
+            else ->
+                fib_non_tail_recursive(i - 1) +
+                    fib_non_tail_recursive(i - 2)
+        }
 
-    "fib" should {
-        "!return the nth fibonacci number" {
+    fun verifyFib(name: String, functionToTest: (Int) -> Int) {
+        val millis = measureTimeMillis {
             persistentMapOf(
                 1 to 1,
                 2 to 1,
@@ -23,10 +36,26 @@ class Exercise1 : WordSpec({
                 5 to 5,
                 6 to 8,
                 7 to 13,
-                8 to 21
+                8 to 21,
+                40 to 102334155
             ).forEach { (n, num) ->
-                fib(n) shouldBe num
+                functionToTest(n) shouldBe num
             }
+        }
+        println("$name: $millis ms")
+    }
+
+    //end::init[]
+
+    "fib" should {
+        "return the nth fibonacci number" {
+            verifyFib("fib", ::fib)
+        }
+    }
+    "fib_non_tail_recursive" should {
+        "return the nth fibonacci number" {
+            verifyFib("fib_non_tail_recursive", ::fib_non_tail_recursive)
         }
     }
 })
+
