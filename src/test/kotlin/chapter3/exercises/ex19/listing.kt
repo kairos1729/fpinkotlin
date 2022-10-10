4fb77195e207a6bbc823da5c1f343531df2f55c8
dominic.godwin <dominic.godwin@rea-group.com>
2022-10-10 20:42:09 +1100

    Ex 3.15 - 22

--- src/test/kotlin/chapter3/exercises/ex19/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex19/listing.kt
@@ -1,20 +1,22 @@
 package chapter3.exercises.ex19
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.exercises.ex14.concat
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A, B> flatMap(xa: List<A>, f: (A) -> List<B>): List<B> =
+    concat(foldRight(xa, List.empty(), {x, t -> Cons(f(x), t) }))
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise19 : WordSpec({
     "list flatmap" should {
-        "!map and flatten a list" {
+        "map and flatten a list" {
             val xs = List.of(1, 2, 3)
             flatMap(xs) { i -> List.of(i, i) } shouldBe
                 List.of(1, 1, 2, 2, 3, 3)
