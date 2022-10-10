f7075415f72c94e9d88e7aa7512373fcfc0faa83
dominic.godwin <dominic.godwin@rea-group.com>
2022-10-10 19:51:02 +1100

    Ex 3.12, 13

--- src/test/kotlin/chapter3/exercises/ex12/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex12/listing.kt
@@ -1,24 +1,31 @@
 package chapter3.exercises.ex12
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldLeft
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
+
+// Got this completely wrong! See the proper solution.
 
 // tag::init[]
 fun <A, B> foldLeftR(xs: List<A>, z: B, f: (B, A) -> B): B =
-
-    SOLUTION_HERE()
+    foldRight(xs.reverse(), z, { a, b -> f(b, a) })
 
 fun <A, B> foldRightL(xs: List<A>, z: B, f: (A, B) -> B): B =
+    foldLeft(xs.reverse(), z, { b, a -> f(a, b) })
 
-    SOLUTION_HERE()
+
+fun <A> reverse(xs: List<A>): List<A> =
+    foldLeftR(xs, List.empty(), { acc, a -> Cons(a, acc) })
+
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise12 : WordSpec({
     "list foldLeftR" should {
-        "!implement foldLeft functionality using foldRight" {
+        "implement foldLeft functionality using foldRight" {
             foldLeftR(
                 List.of(1, 2, 3, 4, 5),
                 0,
@@ -27,11 +34,18 @@ class Exercise12 : WordSpec({
     }
 
     "list foldRightL" should {
-        "!implement foldRight functionality using foldLeft" {
+        "implement foldRight functionality using foldLeft" {
             foldRightL(
                 List.of(1, 2, 3, 4, 5),
                 0,
                 { x, y -> x + y }) shouldBe 15
+        }
+    }
+
+    "list reverse" should {
+        "reverse list elements" {
+            reverse(List.of(1, 2, 3, 4, 5)) shouldBe
+                List.of(5, 4, 3, 2, 1)
         }
     }
 })
