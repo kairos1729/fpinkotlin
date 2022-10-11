7ed60908baa481623c3b2bb103214e6c35779556
dominic.godwin <dominic.godwin@rea-group.com>
2022-10-11 20:41:24 +1100

    Ex 3.24-28

--- src/test/kotlin/chapter3/exercises/ex24/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex24/listing.kt
@@ -5,18 +5,20 @@ import chapter3.Leaf
 import chapter3.Tree
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A> size(tree: Tree<A>): Int =
+    when (tree) {
+        is Leaf -> 1
+        is Branch -> size(tree.left) + 1 + size(tree.right)
+    }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise24 : WordSpec({
     "tree size" should {
-        "!determine the total size of a tree" {
+        "determine the total size of a tree" {
             val tree =
                 Branch(
                     Branch(Leaf(1), Leaf(2)),
--- src/test/kotlin/chapter3/exercises/ex28/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex28/listing.kt
@@ -5,28 +5,26 @@ import chapter3.Leaf
 import chapter3.Tree
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A, B> fold(ta: Tree<A>, l: (A) -> B, b: (B, B) -> B): B =
-
-    SOLUTION_HERE()
+    when (ta) {
+        is Leaf -> l(ta.value)
+        is Branch -> b(fold(ta.left, l, b), fold(ta.right, l, b))
+    }
 
 fun <A> sizeF(ta: Tree<A>): Int =
-
-    SOLUTION_HERE()
+    fold(ta, { 1 }, { l, r -> 1 + l + r })
 
 fun maximumF(ta: Tree<Int>): Int =
-
-    SOLUTION_HERE()
+    fold(ta, { it }, { l, r -> maxOf(l, r) })
 
 fun <A> depthF(ta: Tree<A>): Int =
-
-    SOLUTION_HERE()
+    fold(ta, { 0 }, { l, r -> 1 + maxOf(l, r) })
 
 fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+    fold(ta, { Leaf(f(it)) }, { l: Tree<B>, r: Tree<B> ->  Branch(l, r) })
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
@@ -46,19 +44,19 @@ class Exercise28 : WordSpec({
                 )
             )
         )
-        "!generalise size" {
+        "generalise size" {
             sizeF(tree) shouldBe 15
         }
 
-        "!generalise maximum" {
+        "generalise maximum" {
             maximumF(tree) shouldBe 21
         }
 
-        "!generalise depth" {
+        "generalise depth" {
             depthF(tree) shouldBe 5
         }
 
-        "!generalise map" {
+        "generalise map" {
             mapF(tree) { it * 10 } shouldBe
                 Branch(
                     Branch(Leaf(10), Leaf(20)),
