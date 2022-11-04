b8fcedb6691a72b97ebc0921ce26d55f05d09636
dominic.godwin <dominic.godwin@rea-group.com>
2022-11-05 09:24:18 +1100

    Ex 4.3 - methods using Kotlin's built-in nullables - more succinct and easier to understand, why can't we use these??

--- src/test/kotlin/chapter4/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex3/listing.kt
@@ -9,6 +9,9 @@ import io.kotlintest.specs.WordSpec
 //tag::init[]
 fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
     a.flatMap { sa -> b.map { sb -> f(sa, sb) } }
+
+fun <A, B, C> map2_null(an: A?, bn: B?, f: (A, B) -> C): C? =
+    an?.let { a -> bn?.let { b -> f(a, b) } }
 //end::init[]
 
 //TODO: Enable tests by removing `!` prefix
@@ -19,6 +22,9 @@ class Exercise3 : WordSpec({
         val a = Some(5)
         val b = Some(20)
         val none = Option.empty<Int>()
+        val an = 5
+        val bn = 20
+        val nulld: Int? = null
 
         "combine two option values using a binary function" {
             map2(a, b) { aa, bb ->
@@ -34,6 +40,22 @@ class Exercise3 : WordSpec({
             map2(none, b) { aa, bb ->
                 aa * bb
             } shouldBe None
+        }
+
+        "combine two nullable values using a binary function" {
+            map2_null(an, bn) { aa, bb ->
+                aa * bb
+            } shouldBe 100
+        }
+
+        "return null if either nullable is null" {
+            map2_null(an, nulld) { aa, bb ->
+                aa * bb
+            } shouldBe null
+
+            map2_null(nulld, bn) { aa, bb ->
+                aa * bb
+            } shouldBe null
         }
     }
 })
