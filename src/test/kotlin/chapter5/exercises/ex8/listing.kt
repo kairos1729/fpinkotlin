8f3d92d890f1dc191e0e388ffba7025752852ca2
dominic.godwin <dominic.godwin@rea-group.com>
2022-12-11 16:27:08 +1100

    Ex 8, 9, 10

--- src/test/kotlin/chapter5/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex8/listing.kt
@@ -2,23 +2,22 @@ package chapter5.exercises.ex8
 
 import chapter3.List
 import chapter5.Stream
-import chapter5.toList
+import chapter5.Stream.Companion.cons
 import chapter5.solutions.ex13.take
+import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise8 : WordSpec({
 
     //tag::init[]
     fun <A> constant(a: A): Stream<A> =
-
-        SOLUTION_HERE()
+        cons({ a }, { constant(a) })
     //end::init[]
 
     "constants" should {
-        "!return an infinite stream of a given value" {
+        "return an infinite stream of a given value" {
             constant(1).take(5).toList() shouldBe
                 List.of(1, 1, 1, 1, 1)
         }
