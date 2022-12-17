25707ddaeb524845cebe7fdba16a7c6211006977
dominic.godwin <dominic.godwin@rea-group.com>
2022-12-17 16:30:07 +1100

    Ex 12

--- src/test/kotlin/chapter5/exercises/ex12/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex12/listing.kt
@@ -1,62 +1,66 @@
 package chapter5.exercises.ex12
 
 import chapter3.List
+import chapter4.Some
 import chapter5.Stream
-import chapter5.toList
 import chapter5.solutions.ex13.take
+import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise12 : WordSpec({
 
     //tag::fibs[]
     fun fibs(): Stream<Int> =
-
-        SOLUTION_HERE()
+        Stream.unfold(Pair(0, 1)) { (current, next) ->
+            Some(
+                Pair(
+                    current,
+                    Pair(next, current + next)
+                )
+            )
+        }
     //end::fibs[]
 
     //tag::from[]
     fun from(n: Int): Stream<Int> =
-
-        SOLUTION_HERE()
+        Stream.unfold(n) { current -> Some(Pair(current, current + 1)) }
     //end::from[]
 
     //tag::constant[]
     fun <A> constant(n: A): Stream<A> =
+        Stream.unfold(n) { Some(Pair(it, it)) }
 
-        SOLUTION_HERE()
     //end::constant[]
 
     //tag::ones[]
-    fun ones(): Stream<Int> =
+    fun ones(): Stream<Int> = constant(1)
 
-        SOLUTION_HERE()
     //end::ones[]
 
     "fibs" should {
-        "!return a Stream of fibonacci sequence numbers" {
+        "return a Stream of fibonacci sequence numbers" {
             fibs().take(10).toList() shouldBe
                 List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
         }
     }
 
     "from" should {
-        "!return a Stream of ever incrementing numbers" {
+        "return a Stream of ever incrementing numbers" {
             from(5).take(5).toList() shouldBe List.of(5, 6, 7, 8, 9)
         }
     }
 
     "constants" should {
-        "!return an infinite stream of a given value" {
+        "return an infinite stream of a given value" {
             constant(1).take(5).toList() shouldBe
                 List.of(1, 1, 1, 1, 1)
         }
     }
 
     "ones" should {
-        "!return an infinite stream of 1s" {
+        "return an infinite stream of 1s" {
             ones().take(5).toList() shouldBe List.of(1, 1, 1, 1, 1)
         }
     }
