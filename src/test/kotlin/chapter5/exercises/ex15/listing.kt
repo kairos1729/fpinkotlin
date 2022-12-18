61f6d27920dc103c0367a107b5ea409cbb626338
dominic.godwin <dominic.godwin@rea-group.com>
2022-12-18 18:17:19 +1100

    Ex 15 and 16. 16 not done to efficiency requirements.

--- src/test/kotlin/chapter5/exercises/ex15/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex15/listing.kt
@@ -1,11 +1,17 @@
 package chapter5.exercises.ex15
 
 import chapter3.List
+import chapter4.None
+import chapter4.Option
+import chapter4.Some
+import chapter4.map
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
+import chapter5.exercises.ex11.unfold
 import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 import chapter3.Cons as ConsL
 import chapter3.Nil as NilL
 
@@ -14,16 +20,23 @@ class Exercise15 : WordSpec({
 
     //tag::tails[]
     fun <A> Stream<A>.tails(): Stream<Stream<A>> =
-
-        SOLUTION_HERE()
+        unfold<Stream<A>, Option<Stream<A>>>(Some(this)) {
+            when (it) {
+                is None -> None
+                is Some -> when (val s = it.get) {
+                    is Cons -> Some(s to Some(s.tail()))
+                    is Empty -> Some(s to None)
+                }
+            }
+        }
     //end::tails[]
 
-    fun <A, B> List<A>.map(f: (A) -> B): List<B> =
-
-        SOLUTION_HERE()
+    // fun <A, B> List<A>.map(f: (A) -> B): List<B> =
+    //
+    //     SOLUTION_HERE()
 
     "Stream.tails" should {
-        "!return the stream of suffixes of the input sequence" {
+        "return the stream of suffixes of the input sequence" {
             Stream.of(1, 2, 3)
                 .tails()
                 .toList()
--- src/test/kotlin/chapter5/exercises/ex16/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex16/listing.kt
@@ -1,23 +1,33 @@
 package chapter5.exercises.ex16
 
 import chapter3.List
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
 import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise16 : WordSpec({
 
     //tag::scanright[]
+    // This differes from the real solution. I guess I haven't made my solution efficient...
     fun <A, B> Stream<A>.scanRight(z: B, f: (A, () -> B) -> B): Stream<B> =
+        when (this) {
+            is Cons -> Stream.cons({
+                this.foldRight({ z }) { a, b ->
+                    f(a, b)
+                }
+            }, { this.tail().scanRight(z, f) })
 
-        SOLUTION_HERE()
+            is Empty -> Stream.cons({ z }, { Stream.empty() })
+        }
+
     //end::scanright[]
 
     "Stream.scanRight" should {
-        "!behave like foldRight" {
+        "behave like foldRight" {
             Stream.of(1, 2, 3)
                 .scanRight(0, { a, b ->
                     a + b()
